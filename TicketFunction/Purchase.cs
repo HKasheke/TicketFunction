03c11f3136e06b60a370baefb2a324defe225b46
HKasheke <hope.kasheke@gmail.com>
2025-04-10 13:06:37 -0300

    removed first and last name and put name

--- TicketFunction/Purchase.cs
+++ TicketFunction/Purchase.cs
@@ -11,8 +11,7 @@ namespace TicketFunction
     {
       public int ConcertId { get; set; }
       public string Email { get; set; } = string.Empty;
-      public string FirstName { get; set; } = string.Empty;
-      public string LastName { get; set; } = string.Empty;
+      public string Name { get; set; } = string.Empty;
       public string Phone { get; set; } = string.Empty;
       public int Quantity { get; set; }
       public string CreditCard { get; set; } = string.Empty;
